06bb6feb72ca7488a3938379ba1cc59a6ca76a63
Aureo-Bueno <aureoalexandre@outlook.com>
2023-01-23 20:05:32 -0300

    adicionando componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import Description from './components/Description';
 
 function App() {
+  const name = 'Aureo Bueno'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alterando o Jsx</h1>
+      <p>Ola, {name}</p>
+      <Hello />
+      <Description />
     </div>
   );
 }
---
+++ src/components/Description.js
@@ -0,0 +1,7 @@
+export default function Description() {
+    return (
+        <main>
+            <h2>Componente de descricao</h2>
+        </main>
+    )
+}
