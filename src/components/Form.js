35b9050ca6c81f532124f5d7372144a0a683e7ff
Aureo-Bueno <aureoalexandre@outlook.com>
2023-01-23 20:42:03 -0300

    add useState for form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 export default function Form() {
+
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
 
     function createUser(e) {
         e.preventDedault();
-        alert(`Cadastrou o usuario!`);
+        alert(`Cadastrou o usuario! Nome: ${name}, Senha: ${password} `);
     }
 
     return (
@@ -10,7 +15,12 @@ export default function Form() {
             <h1>Cadastro</h1>
             <form onSubmit={createUser}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome..." required />
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" id="name" name="name" placeholder="Digite seu nome..." required onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" name="password" placeholder="Digite sua senha..." required onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
